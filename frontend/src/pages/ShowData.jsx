1e4956bc5b23a3cf064ee5325fe5b396efdf852e
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-05-12 20:27:31 +0600

    fix the Show data page issue

--- frontend/src/pages/ShowData.jsx
+++ frontend/src/pages/ShowData.jsx
@@ -54,7 +54,7 @@ const ShowData = () => {
         <div className='flex justify-center py-10 gap-4'>
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">All Result is-</h5>
                 </a>
                 <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">The Number of Words is: {word}</p>
                 <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">The Number of Characters is: {character}</p>
